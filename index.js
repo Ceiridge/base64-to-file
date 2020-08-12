3f4c367c99c5b456a53a69051e80271e5f846a7a
Ceiridge <40047520+Ceiridge@users.noreply.github.com>
2020-08-12 19:42:16 +0200

    Reduced output

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ var encodedString = core.getInput('encodedString');
 // most @actions toolkit packages have async methods
 async function run() {
   try { 
-    console.log(process.env);
+    //console.log(process.env);
     const tempFile = Buffer.from(encodedString, 'base64');
     
     if (tempFile.length == 0)
@@ -24,7 +24,7 @@ async function run() {
     core.setOutput('filePath', fileName);
   } 
   catch (error) {
-    core.setFailed(error.message);
+    core.setFailed("Fail");
   }
 }
 
